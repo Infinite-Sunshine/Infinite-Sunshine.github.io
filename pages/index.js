b0c7edf5b73a680daa6bdf85a7c445c72756fff4
Matt Rose <matthew.rose@gmail.com>
2021-05-24 20:25:37 -0500

    initial deploy of NextJS app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+export default function Home() {
+    return (
+        <Flex justifyContent="center">
+            <Heading>The Future Home of Infinite Sunshine</Heading>
+        </Flex>
+    );
+}
