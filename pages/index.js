aeb6eb0cd1cb12c1c86d54d5906be1a4d2b16fa5
Matt Rose <matthew.rose@gmail.com>
2021-05-24 21:04:16 -0500

    Initial updates to landing page to test styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, useColorMode } from "@chakra-ui/react";
 
 export default function Home() {
+    const { toggleColorMode } = useColorMode()
+
     return (
         <Flex justifyContent="center">
-            <Heading>The Future Home of Infinite Sunshine</Heading>
+            <Flex direction="column">
+                <Heading mb={6}>The Future Home of Infinite Sunshine</Heading>
+                <Button onClick={toggleColorMode}>Change Theme</Button>
+            </Flex>
         </Flex>
     );
 }
