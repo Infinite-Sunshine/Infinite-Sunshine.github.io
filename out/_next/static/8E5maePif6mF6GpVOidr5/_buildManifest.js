e2d1b3edfad79388f108e42b336af20ff0ee55d1
Matt Rose <matthew.rose@gmail.com>
2021-05-24 21:29:43 -0500

    remove & ignore out/ and delete NextJS config

--- out/_next/static/8E5maePif6mF6GpVOidr5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-082a1e09e823c3a57e88.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
